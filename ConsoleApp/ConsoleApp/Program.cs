93172ddae4e3182d60dd3952e4581b307493a31a
mmsathish <114723885+mmsathish@users.noreply.github.com>
2023-04-08 22:19:58 +0530

    Add files via upload

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hellow World");
+        }
+    }
+}
