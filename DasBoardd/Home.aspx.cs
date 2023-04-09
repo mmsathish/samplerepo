66bb3e1729596eff63ba1a3909d7113a68c820c4
mmsathish <114723885+mmsathish@users.noreply.github.com>
2023-04-09 11:34:04 +0530

    Add files via upload

---
+++ DasBoardd/Home.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DasBoardd
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Label3.Text = TextBox1.Text.ToString();
+        }
+    }
+}
